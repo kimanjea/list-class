c73b342a3cb581f30fae89bc2ec9739ddc6b15f3
Azhar Amir Kimanje <77221185+kimanjea@users.noreply.github.com>
2023-12-12 01:59:52 -0800

    List class object experiment
    Successful

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization;
+
+namespace Objection {
+
+    class Program {
+    static void Main(string [] args) {
+
+        
+            // we create 3 new objects of class objectlist
+        var azhar = new ObjectList(19, "azhar"); 
+        var najib = new ObjectList(25, "najib");
+        var pink = new ObjectList(20, "pink");
+
+        //We create a list of type Objectlist to hold the objects
+        var player = new List<ObjectList>{};
+
+        //we add our object instances into the class
+        player.Add(pink);
+        player.Add(najib);
+        player.Add(azhar);
+
+        foreach (var item in player)
+        {
+            Console.WriteLine($"Player name: {item.name}"); //we print out our list of players
+            Console.WriteLine($"PLayer age: {item.age}");
+        }
+        
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        player.Remove(pink); //we remove a player
+
+        foreach (var item in player)
+        {
+            Console.WriteLine($"Player name{item.name}"); // we print out our new list of players
+            Console.WriteLine($"Player age: {item.age}");
+        }
+
+    }
+    }
+
+    class ObjectList{ // class that creates a datatype of properties name and age
+        public int age {get; set;}
+
+        public string name { get;set;}
+
+        public ObjectList(int ages, string names) { //constructor
+            age = ages;
+            name = names;
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # list class
  class objects instances in a list
+# ObjectList Project
+* Overview
+The ObjectList project is a simple C# console application that demonstrates the use of a custom class called ObjectList to create objects with properties for age and name. It includes a basic implementation of a player list, allowing you to create, add, and remove players.
+
+
+* Usage
+The ObjectList class is designed to store information about players, including their age and name. The Program class demonstrates the creation, addition, and removal of player objects in a list.
+
+Creating Players:
+*** var azhar = new ObjectList(19, "azhar");
+var najib = new ObjectList(25, "najib");
+var pink = new ObjectList(20, "pink"); ***
+
+
+Adding Players to the List:
+***var player = new List<ObjectList>{};
+player.Add(pink);
+player.Add(najib);
+player.Add(azhar);***
+
+Displaying Player Information:
+***foreach (var item in player)
+{
+    Console.WriteLine($"Player name: {item.name}");
+    Console.WriteLine($"Player age: {item.age}");
+}***
+
+Removing a Player:
+***player.Remove(pink);***
+
+Displaying Updated Player List:
+***foreach (var item in player)
+{
+    Console.WriteLine($"Player name: {item.name}");
+    Console.WriteLine($"Player age: {item.age}");
+}***
+
+
